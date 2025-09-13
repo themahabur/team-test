e3b25d9612d830b0206ca08682685124526ae7f2
ana-yet <anayethusenaj@gmail.com>
2025-09-13 11:30:37 +0600

    sakib page updated

--- src/app/sakib/page.jsx
+++ src/app/sakib/page.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Sakib = () => {
-    return (
-        <div>
-            <h1>Sakib's Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-blue-50 mx-auto py-6">
+        Sakib's Page
+      </h1>
+      <p className="text-2xl text-shadow-amber-500 text-center my-6">
+        Welcome to Sakib's page!
+      </p>
+    </div>
+  );
 };
 
 export default Sakib;
