59344c9979a2d6425f229d279222ec95b3a262f1
Arifuzzaman01 <arifuzzamanar29@gmail.com>
2025-09-13 13:25:54 +0600

    test

---
+++ src/app/arifuzzaman/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <h1 className='text-5xl text-red-400 font-bold text-center'>Hello Everyone, I am Arifuzzaman</h1>
+    </div>
+  )
+}
---
+++ src/app/sakib/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Sakib = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center text-blue-50 mx-auto py-6">
+        Sakib's Page
+      </h1>
+      <p className="text-2xl text-shadow-amber-500 text-center my-6">
+        Welcome to Sakib's page!
+      </p>
+      <p className="text-lg text-center my-4">
+        This is a sample page created to demonstrate routing in Next.js.
+      </p>
+    </div>
+  );
+};
+
+export default Sakib;
